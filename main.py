fc43119e8eb70a4de2df46916dcbc79e0fbaacf2
Jesús Antonio Zúñiga Arce <jzuniga@uabcs.mx>
2025-09-20 01:55:16 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from views.login_view import LoginView
+
+if __name__ == "__main__":
+    login_view = LoginView()
+    login_view.mainloop()
---
+++ views/login_view.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+class LoginView(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Iniciar sesión")
+        self.geometry("400x230")
+        self.resizable(width=False, height=False)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        self.username_label = tk.Label(self, text="Usuario:")
+        self.username_entry = tk.Entry(self)
+        self.password_label = tk.Label(self, text="Password")
+        self.password_entry = tk.Entry(self, show="*")
+        self.login_button = tk.Button(self, text="Iniciar sesión",command=self.login_button_clicked)
+
+        self.username_label.grid(row=0, column=0, padx=10, pady=10, sticky="e")
+        self.username_entry.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+        self.password_label.grid(row=1, column=0, padx=10, pady=10, sticky="e")
+        self.password_entry.grid(row=1, column=1, padx=10, pady=10, sticky="w")
+        self.login_button.grid(row=2, column=0, columnspan=2, pady=10)
+
+    def login_button_clicked(self):
+        print("Click")
